0be4e51625588070f86584294f8c2f4c0d4e7842
Mikhail Vysotskiy <wee.sots.key@gmail.com>
2018-09-10 14:38:19 +0300

    Добавил возможность выбора скорости и порта из списка предложенных

--- src/com_connector.py
+++ src/com_connector.py
@@ -5,12 +5,15 @@ import serial
 # не мешай гуи с коннекторами и тд
 # чтобы из одного класса ты мог менять поля другого тут уже надо повыебываться с колбеками (callback)
 # или встроенными фичами кутэ я не ебу) В си шарпе я хуярил ссылку на метод через (забыл название лол)
-class ComConnector:
+class ComConnector():
     def __init__(self):
         self.session = None
 
-    def connect(self):
-        self.session = serial.Serial('/dev/cu.wchusbserial1410', 9600)
+    def _connect(self, com, baudrate):
+        self.session = serial.Serial(com, baudrate)
         text = self.session.readline(10)
         text_str = text.decode("utf-8")
         return self.session.portstr + ":  " + text_str
+
+    com_lst = ['/dev/cu.wchusbserial1410', '/dev/cu.wchusbserial1430', 'под_винду1', 'под_винду2']
+    baudrate_lst = ["9600", "115200"]
--- src/window.py
+++ src/window.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QTextEdit, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QTextEdit, QVBoxLayout, QLabel, QComboBox
 
 from src.com_connector import ComConnector
 
@@ -25,24 +25,45 @@ class ConnectorWindow(QWidget):
             raise AssertionError('Connector is not instance of ComConnector')
         self.connector = connector
 
+
+        # текущие значения этих параметров
+
+        self.current_com = ComConnector.com_lst[0]
+        self.current_baudrate = ComConnector.baudrate_lst[0]
+
         # переменные лучше называть так: тип_уточнение, но цэ не критично
         # судя по гуидам, к классу формы все элементы сначала доабвляются как атрибуты класса
         # (не как в виндус формс лул)
         # и не юзай кавычки, если ток строка не включает в себя апострафы
         self.button_clear = QPushButton('Очистить поле', self)
         self.button_connect = QPushButton('Подключиться')
+        self.lbl = QLabel("Выберите скорость: ", self)
+        self.lbl1 = QLabel('Выберите порт: ', self)
+        self.combo = QComboBox(self)
+        self.combo1 = QComboBox(self)
+        self.combo.addItems(ComConnector.baudrate_lst)
+        self.combo1.addItems(ComConnector.com_lst)
         # эта хуйня не работала без лямбды, потому что ты передавал резульат выполнения функции
         # а надо было передавать ссылку на саму функцию. Ссылка на функцию - ее имя, без скобачек ()
         self.button_connect.clicked.connect(self._on_button_connect_click)
+        self.combo.activated[str].connect(self._onActivated)
+        self.combo1.activated[str].connect(self._onActivated1)
+        self.button_clear.clicked.connect(self._on_button_connect_clear)
+
 
         layout = QVBoxLayout(self)
         self.text_edit_main = QTextEdit(self)
+        layout.addWidget(self.lbl)
+        layout.addWidget(self.combo)
+        layout.addWidget(self.lbl1)
+        layout.addWidget(self.combo1)
         layout.addWidget(self.text_edit_main)
         layout.addWidget(self.button_connect)
         layout.addWidget(self.button_clear)
 
+
         self.setLayout(layout)
-        self.setGeometry(300, 300, 300, 300)
+        self.setGeometry(400, 400, 400, 400)
         self.setWindowTitle('Serial Port Monitor')
         self.show()
 
@@ -55,11 +76,24 @@ class ConnectorWindow(QWidget):
         self.text_edit_main.append('**Выполняется соединение.....')
         # не пихай в трай-експепт все подряд, это нагружает дохуя, ебашь ток важные "опасные" операции
         try:
-            self.text_edit_main.append(self.connector.connect())
+            self.text_edit_main.append(self.connector._connect(self.current_com, self.current_baudrate))
         except Exception:
             self.text_edit_main.append('**Не удалось законектиться с COM устройством')
             self.text_edit_main.append('**Подключите устройство к порту USB')
 
 
     def _on_button_connect_clear(self):
-       pass
+       print(ComConnector.baudrate_lst[0])
+
+
+    def _onActivated(self, text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
+        self.current_baudrate = text
+        print(self.current_baudrate)
+
+    def _onActivated1(self, text):
+        self.lbl1.setText(text)
+        self.lbl1.adjustSize()
+        self.current_com = text
+        print(self.current_com)
